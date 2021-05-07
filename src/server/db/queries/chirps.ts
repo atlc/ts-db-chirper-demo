03fec91d169900b7855b97e82953ea263e7705c7
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2021-05-06 19:49:37 -0500

    Adding in CRUD router operations to complete the backend

--- src/server/db/queries/chirps.ts
+++ src/server/db/queries/chirps.ts
@@ -1,18 +1,18 @@
 import { Chirps, Users } from '../../../utilities/types';
 import { Query } from '../index';
 
-const get_all = () => Query<Chirps[]>('SELECT * FROM Chirps');
-const get_by_id = (id: Chirps['id']) => Query<Chirps[]>('SELECT * FROM Chirps WHERE id=?', [id]);
+// const get_all = () => Query<Chirps[]>('SELECT * FROM Chirps');
+// const get_by_id = (id: Chirps['id']) => Query<Chirps[]>('SELECT * FROM Chirps WHERE id=?', [id]);
 const get_with_authors = () => Query<(Chirps & Users)[]>('SELECT c.*, u.username FROM Chirps c JOIN Users u ON user_id=u.id');
-const get_by_id_with_author = (id: Chirps['id']) => Query<(Chirps & Users)[]>('SELECT c.*, u.username FROM Chirps c JOIN Users u ON user_id=u.id WHERE u.id=?', [id]);
+const get_by_id_with_author = (id: Chirps['id']) => Query<(Chirps & Users)[]>('SELECT c.*, u.username FROM Chirps c JOIN Users u ON user_id=u.id WHERE c.id=?', [id]);
 
 const create = ({ ...chirp }: Chirps) => Query('INSERT INTO Chirps SET ?', [chirp]);
 const update = ({ id, ...chirp }: Chirps) => Query('UPDATE Chirps SET ? WHERE id=?', [chirp, id]);
 const destroy = (id: Chirps['id']) => Query('DELETE FROM Chirps WHERE id=?', [id]);
 
 export default {
-    get_all,
-    get_by_id,
+    // get_all,
+    // get_by_id,
     get_with_authors,
     get_by_id_with_author,
     create,
---
+++ src/server/routes/chirps.ts
@@ -0,0 +1,83 @@
+import * as express from 'express';
+import { v4 as uuidv4 } from 'uuid';
+
+import db from '../db';
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    try {
+        const allChirps = await db.Chirps.get_with_authors();
+        res.json(allChirps);
+    } catch (error) {
+        res.status(500).json({ message: "An unknown error occurred!", error });
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        const [singleChirp] = await db.Chirps.get_by_id_with_author(id);
+        res.json(singleChirp);
+    } catch (error) {
+        res.status(500).json({ message: "An unknown error occurred!", error });
+    }
+});
+
+
+router.post('/', async (req, res) => {
+    const id = uuidv4();
+    const newChirp = {
+        id,
+        ...req.body
+    };
+
+    try {
+        console.log(newChirp)
+        const dbResponse = await db.Chirps.create(newChirp);
+
+        if (dbResponse.errno) {
+            res.status(500).json({ message: "There was an error creating the chirp", error: dbResponse.sqlMessage });
+        } else {
+            res.status(201).json({ message: "The chirp was created successfully!", newChirpId: id });
+        }
+    } catch (error) {
+        res.status(500).json({ message: "An unknown error occurred!", error });
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    const id = req.params.id;
+    const chirp = req.body;
+
+    try {
+        const dbResponse = await db.Chirps.update({ id, ...chirp });
+
+        if (dbResponse.errno) {
+            res.status(500).json({ message: "There was an error updating the chirp", error: dbResponse.sqlMessage });
+        } else {
+            res.status(201).json({ message: "The chirp was updated successfully!" });
+        }
+    } catch (error) {
+        res.status(500).json({ message: "An unknown error occurred!", error });
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    const id = req.params.id;
+
+    try {
+        const dbResponse = await db.Chirps.destroy(id);
+
+        if (dbResponse.errno) {
+            res.status(500).json({ message: "There was an error deleting the chirp", error: dbResponse.sqlMessage });
+        } else {
+            res.status(201).json({ message: "The chirp was deleted successfully!" });
+        }
+    } catch (error) {
+        res.status(500).json({ message: "An unknown error occurred!", error });
+    }
+});
+
+
+export default router;
